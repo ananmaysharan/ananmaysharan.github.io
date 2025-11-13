8be8ea8b12de205ae0726f0679f3ec97b81f08b3
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2025-11-12 23:46:54 -0500

    new about and case study

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,53 +1,64 @@
 <script>
     import Listening from "$lib/components/Listening.svelte";
     import Button from "$lib/components/Button.svelte";
-    import profile from '$lib/assets/profile.jpg'
+    import profile from "$lib/assets/profile.jpg";
 </script>
 
-
-<div class='text' id='about-text'>
-
-    <img src={profile} alt='profile' id="profile">
+<div class="text" id="about-text">
+    <img src={profile} alt="profile" id="profile" />
 
     <div>
-<p>
-   I'm currently studying <a href="https://www.gsd.harvard.edu/design-engineering/">Design Engineering</a> at Harvard University, a collaborative degree program between the Graduate School of Design and the School of Engineering and Applied Sciences.
-</p>
-<p>
-   Previously, I worked at the <a href="https://canurb.org/">Canadian Urban Institute</a> as a web developer and data analyst, and interned at <a href="https://thebentway.ca/">The Bentway</a> and <a href="https://www.sodatone.com/">Sodatone (Warner Music Group)</a>.
-</p>
-<p>
-    I graduated from the University of Toronto with a bachelor’s degree in Human Geography, Computer Science and GIS in 2023.
-</p>
-<p>
-    Some of my interests include: urban environments, design, music, fashion, cooking and soccer, particularly in how they interact with technology and digital culture. 
-</p>
-<p>
-    Please reach out to me via email if you’re interested in chatting about any of my projects, past experience or to just to say hi! 
-</p>
+        <p>
+            I'm currently studying <a
+                href="https://www.gsd.harvard.edu/design-engineering/"
+                >Design Engineering</a
+            > at Harvard University, a collaborative degree program between the Graduate
+            School of Design and the School of Engineering and Applied Sciences.
+        </p>
+        <p>
+            Previously, I worked at the <a href="https://canurb.org/"
+                >Canadian Urban Institute</a
+            >
+            as a web developer and data analyst, and interned at
+            <a href="https://thebentway.ca/">The Bentway</a>
+            and
+            <a href="https://www.sodatone.com/">Sodatone (Warner Music Group)</a
+            >.
+        </p>
+        <p>
+            I graduated from the University of Toronto with a bachelor’s degree
+            in Human Geography, Computer Science and GIS in 2023.
+        </p>
+        <p>
+            Some of my interests include: urban environments, design, music,
+            fashion, cooking and soccer, particularly in how they interact with
+            technology and digital culture.
+        </p>
+        <p>
+            Please reach out to me via email if you’re interested in chatting
+            about any of my projects, past experience or to just to say hi!
+        </p>
+
+    <Button url={"mailto:ananmay01@gmail.com"} buttonText={"Email"} />
+        </div>
 
 </div>
 
-<Button url={'mailto:ananmay01@gmail.com'} buttonText={'Email'} />
-
-
+<div class="media-container">
+    <Listening />
 </div>
-
-
-<div class='media-container'>
-<Listening/>
-</div>
-
 
 <style>
     img {
         width: 300px;
         height: 100%;
     }
+
+    #about-text {
+        display: flex;
+        flex-direction: row;
+        gap: 4rem;
+        align-items: center;
+        padding: 4rem;
+    }
 </style>
-
-
-
-
-
-
--- src/routes/work/ocean-records/+page.svelte
+++ src/routes/work/ocean-records/+page.svelte
@@ -5,6 +5,7 @@
 
     const sections = [
         { id: "overview", title: "Overview" },
+        // { id: "solution", title: "Solution" },
     ];
 </script>
 
@@ -19,19 +20,61 @@
             link="http://ocean-records.vercel.app/"
             img={mms}
             roles={["Designer", "Web Developer"]}
-            timeline="September 2025"
+            timeline="September 2025 - October 2025"
             technologies={["SvelteKit", "Mapbox", "GSAP", "P5.js"]}
             skills={["Design Research", "Data Visualization", "Web Development"]}
 
         />
 
         <section id="overview">
-            <h2>Overview</h2>
-
+            <h3>Overview</h3>
+            <h2>How can we convey the acoustic conflict between human activity and marine life in Monterey Bay, California?</h2>
             <p>
-                Using machine learning to listen to the ocean.
+                I worked with a partner to research, design and develop this data visualization project, completed as part of my Master's in Design Engineering program at Harvard University.
+                <!-- The requirements were to create an interactive data visualization designed to uncover systems-level constructs, build knowledge and inform decision-making and impact, in response to the theme for the year 2025/26: Artificial + Natural. -->
+            </p>
+            
+            <h4>Problem</h4>
+            <ul>
+                <li>
+                    Ship strikes, which are collisions between vessels and marine animals, are the leading cause of whale deaths worldwide. 
+                </li>
+                <li>
+                    Most ship strikes go unreported, since the animals usually sink to the ocean floor if they die, making it difficult to assess their true impact on marine populations.
+                </li>
+                <li>
+                    Shipping vessels emit similar sound frequencies as whale species, which disrupts their ability to communicate and navigate, making them more prone to strikes.
+                </li>   
+            </ul>
+
+            <h4>Approach</h4>
+            <p>
+            We found a NOAA dataset that contained over 4 years of continuous underwater acoustic recordings from the Monterey Bay National Marine Sanctuary in California. Monterey Bay is home to 36 species of marine mammals and one of the busiest shipping lanes in the world, with over 800,000 gross tons of cargo passing through annually.
+            </p>
+            <p>
+            We used the NOAA data in conjunction with Google DeepMind's machine learning model, Perch 2.0, that can accurately classify different whale species and human-made sounds from underwater acoustic recordings.
+            </p>
+            <p>
+            Our interactive web-based data visualization, Ocean Records, allows users to explore the underwater soundscape of Monterey Bay between 2018 and 2022, highlighting the presence of different whale species and human-made sounds such as ship noise and fishing activity.
             </p>
         </section>
+<!-- 
+        <section id="solution">
+            <h3>Solution</h3>
+            <h4>Map</h4>
+            <h4>Year View</h4>
+            <h4>Month View</h4>
+            <h4>Day View</h4>
+        </section>
+
+        <section id="research">
+            <h3>Research</h3>
+            <h4>Ideation</h4>
+            <h4>Methodology</h4>
+            <h4>Sketches</h4>
+        </section> -->
+
+
 
 
         <!-- <div id="solution" class="splash">
